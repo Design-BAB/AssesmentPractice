eb86240c59b0dfd5b1b6a65084caff287d121b02
Bryan Becerra <87731894+Design-BAB@users.noreply.github.com>
2026-02-20 10:55:43 -0500

    Upload with CSV

--- PythonPractice/main.py
+++ PythonPractice/main.py
@@ -1,6 +1,11 @@
 #Python Warm-up challenges
+
+import csv
+import sqlite3
+from icecream import ic
+
 #reverse a string
-myString = "The quick drown fox jumps over the lazy dog"
+myString = "abcab"
 newString = ""
 lengthOfString = len(myString)
 for x in myString :
@@ -17,6 +22,7 @@ else :
         print("The string is a pangram")
     else :
         print("The string is not a pangram")
+
 #Count the frequency of each character and return the top 3 most common
 letters = {"A": False}
 for x in myString :
@@ -24,9 +30,9 @@ for x in myString :
     z = myString.upper()
     letters.update({y: z.count(y)})
 sortedLetters = dict(sorted(letters.items(), key=lambda item: item[1]))
-sortedLetters.pop(' ')
 lastThreeItems = list(sortedLetters.items())[-3:]
 print(lastThreeItems)
+
 #Fubanacci Sequence
 allFubNums = [1, 1]
 nMinusOne = allFubNums[1]
@@ -40,6 +46,7 @@ while x < 25 :
     nMinusTwo = allFubNums[len(allFubNums) - 2]
     x = x + 1
 print(allFubNums)
+
 #Given a sentence, capitalize the first letter of each word without using .title()
 newString = ""
 CapUpNext = True
@@ -53,3 +60,40 @@ for x in myString :
     else:
         newString = newString + x
 print(newString)
+
+#Find the longest substring without repeating characters
+newString = ""
+bestString = ""
+
+for x in myString:
+    if x in newString:
+        index = newString.index(x)
+        newString = newString[newString.index(x) + 1:] + x
+    else:
+        newString += x
+    
+    if len(newString) > len(bestString):
+        bestString = newString
+
+print("The longest substring without repeating characters is " + bestString)
+
+#CSV reading
+#creating a new list for dicitonaries to write
+newCSVtoWrite = []
+#open file then read as Dictionaries
+with open("cases.csv", 'r') as file:
+    csvreader = csv.DictReader(file)
+    for row in csvreader:
+        #to search for overdue stuff
+        amountNum = float(row["amount_owed"])
+        if amountNum > 1000:
+            newCSVtoAdd = row
+            newCSVtoWrite.append(newCSVtoAdd)
+
+    print(newCSVtoWrite)
+
+with open("bigCases.csv", mode="w", newline="") as csvfile:
+    fieldNames = newCSVtoWrite[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldNames)
+    for row in newCSVtoWrite:
+        writer.writerow(row)
